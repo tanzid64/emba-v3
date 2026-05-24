ff5ee7cebb3e849bca781e6095636fa2541d7ace
MD. TANZID HAQUE <tanzid3@gmail.com>
2026-05-25 00:34:16 +0600

    feat: add degree config with education levels and board options

---
+++ config/degree.php
@@ -0,0 +1,99 @@
+<?php
+
+return [
+    [
+        'label' => 'S.S.C / Equivalent',
+        'description' => 'The applicant has completed their Secondary School Certificate or an equivalent qualification.',
+        'has_options' => false,
+        'board_type' => 'select',
+        'sub_type' => 'select',
+        'subjects' => [
+            'Science',
+            'Humanities',
+            'Business Studies',
+            'Technical / Vocational',
+            'Madrasah',
+            'Others'
+        ],
+        'boards' => [
+            'Dhaka',
+            'Chittagong',
+            'Rajshahi',
+            'Khulna',
+            'Comilla',
+            'Jessore',
+            'Mymensingh',
+            'Rajshahi',
+            'Rangpur',
+            'Barisal',
+            'Sylhet',
+            'Dinajpur',
+            'Madrasah',
+            'Technical',
+            'Others'
+        ],
+    ],
+    [
+        'label' => 'Higher Secondary / Equivalent',
+        'description' => 'The applicant has completed their Higher Secondary Certificate or an equivalent qualification.',
+        'has_options' => false,
+        'board_type' => 'select',
+        'sub_type' => 'select',
+        'subjects' => [
+            'Science',
+            'Humanities',
+            'Business Studies',
+            'Technical / Vocational',
+            'Madrasah',
+            'Others'
+        ],
+        'boards' => [
+            'Dhaka',
+            'Chittagong',
+            'Rajshahi',
+            'Khulna',
+            'Comilla',
+            'Jessore',
+            'Mymensingh',
+            'Rajshahi',
+            'Rangpur',
+            'Barisal',
+            'Sylhet',
+            'Dinajpur',
+            'Madrasah',
+            'Technical',
+            'Others'
+        ],
+    ],
+    [
+        'label' => 'Bachelor\'s Degree',
+        'description' => 'The applicant has completed a Bachelor\'s degree program.',
+        'has_options' => false,
+        'board_type' => 'input',
+        'sub_type' => 'input',
+    ],
+    [
+        'label' => 'Master\'s Degree',
+        'description' => 'The applicant has completed a Master\'s degree program.',
+        'has_options' => false,
+        'board_type' => 'input',
+        'sub_type' => 'input',
+    ],
+    [
+        'label' => 'Other Degree',
+        'description' => 'The applicant has completed a Doctor of Philosophy (Ph.D.) or an equivalent research degree.',
+        'has_options' => true,
+        'board_type' => 'input',
+        'sub_type' => 'select',
+        'options' => [
+            'Doctoral / Ph.D.',
+            'M.Phil.',
+            'Ed.D.',
+            'D.Sc.',
+            'D.Litt.',
+            'Diploma',
+            'MBBS',
+            'CA / CMA'
+        ],
+    ]
+];
